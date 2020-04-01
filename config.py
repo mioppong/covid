9eff83dfa6802d8be664a9a6aa458b0d7d7ef3f5
oppong <michaeloppong123@gmail.com>
2020-03-31 22:15:51 -0400

    making sure config file is gone from peoples eyes

--- config.py
+++
@@ -1,4 +0,0 @@
-api_key = "ZdyBDtHUwKxPTEivm3vvLDvMM"
-api_secret = "NKZgQodmG7gUnR04Sb5Aq03LR7jEHBTku0NIKGCZDqTokBybSW"
-access_token = "979391071-2Nu1tGypbmKQKJHphOMLZsVFRK2KSOvAwXxY54jL"
-token_secret = "OjbmPs95eXosVpzrab0IV7z6RMPooqE0PO3WWqB0p277f"
