6d4109591412a6cb782fa2cc756307be583447c9
oppong <michaeloppong123@gmail.com>
2020-04-02 22:51:40 -0400

    adding sentiment analysis

--- covid.py
+++ covid.py
@@ -2,13 +2,26 @@ import tweepy
 import config
 import pycountry
 from pprint import pprint
+from azure.ai.textanalytics import TextAnalyticsClient, TextAnalyticsApiKeyCredential
+import tweepy
+
+
+
+
+def authenticate_client():
+    ta_credential = TextAnalyticsApiKeyCredential(config.key)
+    text_analytics_client = TextAnalyticsClient(
+            endpoint=config.endpoint, credential=ta_credential)
+    return text_analytics_client
+
+client = authenticate_client()
+
+#-------------------------------------------------------------------------------------------
 
 #authentication needed for twitter
 auth = tweepy.OAuthHandler(config.api_key,config.api_secret)
 auth.set_access_token(config.access_token,config.token_secret)
 api = tweepy.API(auth)
-
-
 class MyStreamListener(tweepy.StreamListener):
     #this method is called everytime there is a tweet
     #basically everytime we see a hashtag, increment each hashtag counter by 1
@@ -25,15 +38,13 @@ class MyStreamListener(tweepy.StreamListener):
             return False
 
 
-countries = ['Canada','Italy']
+countries = list(pycountry.countries)
 
 my_stream_listener = MyStreamListener()
 
 #my_stream = tweepy.Stream(auth, listener=my_stream_listener)
 #my_stream.filter(track=countries,is_async=True)
 
-
-for x in (list(pycountry.countries)):
-    print(x.name)
+print(len(countries))
 
     
